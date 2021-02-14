16c5e59d81925b7e962dcd9f086b7b3b86a70380
Masaaki Kodama <tc003ssr@gmail.com>
2021-02-14 19:23:27 +0900

    initialize flutter project

---
+++ README.md
@@ -0,0 +1,6 @@
+# speed_code_pet_app
+
+UI構築練習用のプロジェクト。
+
+https://github.com/m-kodama/speed_code_pet_app.git
+デザイナはこちらから拝借。
---
+++ android/app/src/main/kotlin/m/kodama/speed_code_pet_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package m.kodama.speed_code_pet_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
