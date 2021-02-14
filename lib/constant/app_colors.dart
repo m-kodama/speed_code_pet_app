cf7c20aa40c02638d19782c63de821b40dfde75f
Masaaki Kodama <tc003ssr@gmail.com>
2021-02-14 20:00:20 +0900

    feat: main.dartの初期化

--- lib/constant/app_colors.dart
+++ lib/constant/app_colors.dart
@@ -1,7 +1,13 @@
+import 'dart:ui';
+
 class AppColors {
-  static const int primary = 0xFFFF5D36;
-  static const int amber = 0xFFFFB048;
-  static const int pink = 0xFFF96B9A;
-  static const int orange = 0xFFF77441;
-  static const int blue = 0xFF5097FF;
+  static const Color primary = Color(0xFFFF5D36);
+  static const Color amber = Color(0xFFFFB048);
+  static const Color pink = Color(0xFFF96B9A);
+  static const Color orange = Color(0xFFF77441);
+  static const Color blue = Color(0xFF5097FF);
+  static const Color background = Color(0xFF181A21);
+  static const Color sheet = Color(0xFF1F2130);
+  static const Color text = Color(0xFFFFFFFF);
+  static Color subText = text.withOpacity(0.2);
 }
