1de48f7be15dc265d85266c94a4d911e75777e0f
Masaaki Kodama <tc003ssr@gmail.com>
2021-02-14 19:48:52 +0900

    feat: 定数クラスを作成

---
+++ lib/constant/app_colors.dart
@@ -0,0 +1,7 @@
+class AppColors {
+  static const int primary = 0xFFFF5D36;
+  static const int amber = 0xFFFFB048;
+  static const int pink = 0xFFF96B9A;
+  static const int orange = 0xFFF77441;
+  static const int blue = 0xFF5097FF;
+}
